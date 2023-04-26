d86469be060b8b1607be5bbf74f6d43ad54293a6
Alex Mootz <alexmootz@hotmail.com>
2023-04-25 21:27:34 -0400

    feat: add PoolConfig LedgerData object

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 export * from './backstop/backstop_contract';
-export * from './pool/pool_contract';
-export * from './token/token_contract';
-
 export { Q4W } from './backstop/Q4W';
+
+export * from './pool/pool_contract';
 export { Reserve, ReserveData, ReserveConfig } from './pool/reserve';
+export { PoolConfig } from './pool/pool_config';
+
+export * from './token/token_contract';
 
 export * as scval_converter from './scval_converter';
 export * as data_entry_converter from './data_entry_converter';
---
+++ src/pool/pool_config.ts
@@ -0,0 +1,43 @@
+import { xdr } from 'stellar-base';
+import { scvalToNumber, scvalToString } from '../scval_converter';
+
+export class PoolConfig {
+  constructor(public bstop_rate: number, public oracle: string, public status: number) {}
+
+  static fromContractDataXDR(xdr_string: string): PoolConfig {
+    const data_entry_map = xdr.LedgerEntryData.fromXDR(xdr_string, 'base64')
+      .contractData()
+      ?.val()
+      .map();
+    if (data_entry_map == undefined) {
+      throw Error('contract data value is not a map');
+    }
+
+    let bstop_rate: number | undefined;
+    let oracle: string | undefined;
+    let status: number | undefined;
+    for (const map_entry of data_entry_map) {
+      switch (map_entry?.key()?.sym()?.toString()) {
+        case 'bstop_rate':
+          bstop_rate = scvalToNumber(map_entry.val());
+          break;
+        case 'oracle':
+          oracle = scvalToString(map_entry.val());
+          break;
+        case 'status':
+          status = scvalToNumber(map_entry.val());
+          break;
+        default:
+          throw Error(
+            `scvMap value malformed: should not contain ${map_entry?.key()?.sym()?.toString()}`
+          );
+      }
+    }
+
+    if (bstop_rate == undefined || oracle == undefined || status == undefined) {
+      throw Error('scvMap value malformed');
+    }
+
+    return new PoolConfig(bstop_rate, oracle, status);
+  }
+}
