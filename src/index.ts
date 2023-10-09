25e94a34c24b063856c99b71ce155b8de6e925c3
Alex Mootz <alexmootz@hotmail.com>
2023-10-09 13:33:32 -0400

    feat: expose ContractResult response method and invocation method

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ export * as PoolFactory from './pool_factory/index.js';
 export * as scval_converter from './scval_converter.js';
 export * as data_entry_converter from './data_entry_converter.js';
 
+export * from './contract_result.js';
+export * from './tx.js';
+
 export type u32 = number;
 export type i32 = number;
 export type u64 = bigint;
@@ -16,63 +19,6 @@ export type i64 = bigint;
 export type u128 = bigint;
 export type i128 = bigint;
 export type Option<T> = T | undefined;
-
-export class ContractResult<T> {
-  hash: string;
-  ok: boolean;
-  value?: T;
-  error?: Error;
-
-  constructor(hash: string, ok: boolean, value?: T, error?: Error) {
-    this.hash = hash;
-    this.ok = ok;
-    this.value = value;
-    this.error = error;
-  }
-
-  /**
-   * Create an Error ContractResult.
-   * @param hash - The hash of the transaction.
-   * @param error - The error
-   * @returns - Contract Result
-   */
-  static error<T>(hash: string, error: Error): ContractResult<T> {
-    return new ContractResult(hash, false, undefined, error);
-  }
-
-  /**
-   * Create a Successful ContractResult.
-   * @param hash - The hash of the transaction.
-   * @param value - The return value, if any.
-   * @returns - Contract Result
-   */
-  static success<T>(hash: string, value?: T): ContractResult<T> {
-    return new ContractResult(hash, true, value, undefined);
-  }
-
-  /**
-   * Unwrap a successful result, or throw an error.
-   *
-   * @returns The value of the result.
-   */
-  unwrap(): T {
-    if (this.ok) {
-      return this.value;
-    } else if (this.error !== undefined) {
-      throw this.error;
-    } else {
-      throw new Error('unable to unwrap ContractResult');
-    }
-  }
-
-  toString(): string {
-    if (this.ok) {
-      return `Success: ${this.value}`;
-    } else {
-      return `Failure: ${this.error}`;
-    }
-  }
-}
 
 export type SorobanResponse =
   | SorobanRpc.SimulateTransactionResponse
