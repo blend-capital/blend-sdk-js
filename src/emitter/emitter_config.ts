7cccad31c944bf4eedb8aa74d7395d12d1a93559
Ryan Yang <ryanyang2001@gmail.com>
2023-10-18 09:19:09 -0500

    emitter: feat: add emitter config fetching and deserializing

---
+++ src/emitter/emitter_config.ts
@@ -0,0 +1,46 @@
+import { Address, xdr, Server, scValToNative } from 'soroban-client';
+import { Network } from '../index.js';
+
+export class EmitterConfig {
+  constructor(public blndTkn: string, public backstop: string) {}
+
+  static async load(network: Network, emitterId: string) {
+    const SorobanRpc = new Server(network.rpc, network.opts);
+    const contractInstanceXDR = xdr.LedgerKey.contractData(
+      new xdr.LedgerKeyContractData({
+        contract: Address.fromString(emitterId).toScAddress(),
+        key: xdr.ScVal.scvLedgerKeyContractInstance(),
+        durability: xdr.ContractDataDurability.persistent(),
+      })
+    );
+
+    let blndTkn: string | undefined;
+    let backstop: string | undefined;
+    const entries_results = (await SorobanRpc.getLedgerEntries(contractInstanceXDR)).entries ?? [];
+    const instance_entry = xdr.LedgerEntryData.fromXDR(entries_results[0].xdr, 'base64')
+      .contractData()
+      .val()
+      .instance()
+      .storage()
+      ?.map((entry) => {
+        const key = entry.key().vec().at(0);
+        switch (key.sym().toString()) {
+          case 'BlendId':
+            blndTkn = Address.fromScVal(entry.val()).toString();
+            return;
+          case 'Backstop':
+            backstop = Address.fromScVal(entry.val()).toString();
+            return;
+          default:
+            throw Error('invalid emitter config entry key');
+        }
+      });
+    if (instance_entry == undefined) {
+      throw Error('Unable to load emitter config');
+    }
+    if (blndTkn == undefined || backstop == undefined) {
+      throw Error('Unable to load emitter config');
+    }
+    return new EmitterConfig(blndTkn, backstop);
+  }
+}
