798d85ffd3c95183dbd4b6aa9b7d02a7aae4477d
Ryan Yang <ryanyang2001@gmail.com>
2023-10-19 15:08:09 -0500

    pool: chore: load reserve list with pool_config and rename class attributes using camel case

--- src/pool/pool_config.ts
+++ src/pool/pool_config.ts
@@ -8,17 +8,25 @@ export class PoolConfig {
     public blndTkn: string,
     public usdcTkn: string,
     public backstop: string,
-    public bstop_rate: number,
+    public backstopRate: number,
     public oracle: string,
-    public status: number
+    public status: number,
+    public reserveList: string[]
   ) {}
 
-  static async load(network: Network, pool_id: string) {
+  static async load(network: Network, poolId: string) {
     const SorobanRpc = new Server(network.rpc, network.opts);
     const contractInstanceXDR = xdr.LedgerKey.contractData(
       new xdr.LedgerKeyContractData({
-        contract: Address.fromString(pool_id).toScAddress(),
+        contract: Address.fromString(poolId).toScAddress(),
         key: xdr.ScVal.scvLedgerKeyContractInstance(),
+        durability: xdr.ContractDataDurability.persistent(),
+      })
+    );
+    const reserveListDataKey = xdr.LedgerKey.contractData(
+      new xdr.LedgerKeyContractData({
+        contract: Address.fromString(poolId).toScAddress(),
+        key: xdr.ScVal.scvSymbol('ResList'),
         durability: xdr.ContractDataDurability.persistent(),
       })
     );
@@ -27,59 +35,95 @@ export class PoolConfig {
     let blndTkn: string | undefined;
     let usdcTkn: string | undefined;
     let backstop: string | undefined;
-    let bstop_rate: number | undefined;
+    let backstopRate: number | undefined;
     let oracle: string | undefined;
     let status: number | undefined;
+    let reserveList: string[] | undefined;
 
-    const entries_results = (await SorobanRpc.getLedgerEntries(contractInstanceXDR)).entries ?? [];
-    const instance_entry = xdr.LedgerEntryData.fromXDR(entries_results[0].xdr, 'base64')
-      .contractData()
-      .val()
-      .instance()
-      .storage()
-      ?.map((entry) => {
-        switch (entry.key().sym().toString()) {
-          case 'Admin':
-            admin = Address.fromScVal(entry.val()).toString();
-            return;
-          case 'BLNDTkn':
-            blndTkn = Address.fromScVal(entry.val()).toString();
-            return;
-          case 'Backstop':
-            backstop = Address.fromScVal(entry.val()).toString();
-            return;
-          case 'USDCTkn':
-            usdcTkn = Address.fromScVal(entry.val()).toString();
-            return;
-          case 'PoolConfig':
-            entry
-              .val()
-              .map()
-              ?.map((config_entry) => {
-                switch (config_entry.key().sym().toString()) {
-                  case 'bstop_rate':
-                    bstop_rate = Number(config_entry.val().u64().toString());
-                    return;
-                  case 'oracle':
-                    oracle = Address.fromScVal(config_entry.val()).toString();
-                    return;
-                  case 'status':
-                    status = scValToNative(config_entry.val());
-                    return;
-                }
-              });
-            if (bstop_rate == undefined || oracle == undefined || status == undefined) {
-              throw new Error();
-            }
-            return;
-          case 'Name':
-            name = entry.val().sym().toString();
-            return;
+    const poolConfigEntries =
+      (await SorobanRpc.getLedgerEntries(contractInstanceXDR, reserveListDataKey)).entries ?? [];
+
+    for (const entry of poolConfigEntries) {
+      const ledgerData = xdr.LedgerEntryData.fromXDR(entry.xdr, 'base64').contractData();
+      let key: xdr.ScVal;
+      switch (ledgerData.key().switch()) {
+        case xdr.ScValType.scvVec():
+          key = ledgerData.key().vec().at(0);
+          break;
+        case xdr.ScValType.scvSymbol():
+          key = ledgerData.key();
+          break;
+        case xdr.ScValType.scvLedgerKeyContractInstance():
+          key = xdr.ScVal.scvSymbol('PoolInstance');
+          break;
+      }
+      switch (key.sym().toString()) {
+        case 'PoolInstance': {
+          const instanceEntry = ledgerData
+            .val()
+            .instance()
+            .storage()
+            ?.map((entry) => {
+              switch (entry.key().sym().toString()) {
+                case 'Admin':
+                  admin = Address.fromScVal(entry.val()).toString();
+                  return;
+                case 'BLNDTkn':
+                  blndTkn = Address.fromScVal(entry.val()).toString();
+                  return;
+                case 'Backstop':
+                  backstop = Address.fromScVal(entry.val()).toString();
+                  return;
+                case 'USDCTkn':
+                  usdcTkn = Address.fromScVal(entry.val()).toString();
+                  return;
+                case 'PoolConfig':
+                  entry
+                    .val()
+                    .map()
+                    ?.map((config_entry) => {
+                      switch (config_entry.key().sym().toString()) {
+                        case 'bstop_rate':
+                          backstopRate = Number(config_entry.val().u64().toString());
+                          return;
+                        case 'oracle':
+                          oracle = Address.fromScVal(config_entry.val()).toString();
+                          return;
+                        case 'status':
+                          status = scValToNative(config_entry.val());
+                          return;
+                      }
+                    });
+                  if (backstopRate == undefined || oracle == undefined || status == undefined) {
+                    throw new Error();
+                  }
+                  return;
+                case 'Name':
+                  name = entry.val().sym().toString();
+                  return;
+              }
+            });
+          if (instanceEntry == undefined) {
+            throw Error('unable to load pool instance');
+          }
+          break;
         }
-      });
-    if (instance_entry == undefined) {
-      throw Error('unable to load pool instance');
+        case 'ResList':
+          reserveList = scValToNative(ledgerData.val());
+          break;
+      }
     }
-    return new PoolConfig(admin, name, blndTkn, usdcTkn, backstop, bstop_rate, oracle, status);
+
+    return new PoolConfig(
+      admin,
+      name,
+      blndTkn,
+      usdcTkn,
+      backstop,
+      backstopRate,
+      oracle,
+      status,
+      reserveList
+    );
   }
 }
