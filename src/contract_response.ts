c095f63706c6ef48798e66397c33ad2e58dd40d6
Ryang-21 <ryanyang2001@gmail.com>
2024-02-29 16:44:24 -0600

    chore: remove resource object

--- src/contract_response.ts
+++ src/contract_response.ts
@@ -42,70 +42,10 @@ export class Err<E extends ContractError = ContractError> implements Result<any,
     return !this.isOk();
   }
 }
-export class Resources {
-  fee: number;
-  refundableFee: number;
-  cpuInst: number;
-  readBytes: number;
-  writeBytes: number;
-  readOnlyEntries: number;
-  readWriteEntries: number;
-
-  constructor(
-    fee: number,
-    refundableFee: number,
-    cpuInst: number,
-    readBytes: number,
-    writeBytes: number,
-    readOnlyEntries: number,
-    readWriteEntries: number
-  ) {
-    this.fee = fee;
-    this.refundableFee = refundableFee;
-    this.cpuInst = cpuInst;
-    this.readBytes = readBytes;
-    this.writeBytes = writeBytes;
-    this.readOnlyEntries = readOnlyEntries;
-    this.readWriteEntries = readWriteEntries;
-  }
-
-  /**
-   * Builds a Resources object from TransactionEnvelope
-   * @returns - Resources
-   */
-  static fromTransaction(tx: xdr.TransactionEnvelope | string): Resources {
-    if (typeof tx === 'string') {
-      tx = xdr.TransactionEnvelope.fromXDR(tx, 'base64');
-    }
-    const transaction = tx.v1().tx();
-    const data = transaction.ext().sorobanData();
-    const sorobanResources = data.resources();
-    const footprint = sorobanResources.footprint();
-
-    const fee = transaction.fee();
-    const refundableFee = Number(data.resourceFee().toString());
-    const cpuInst = sorobanResources.instructions();
-    const readBytes = sorobanResources.readBytes();
-    const writeBytes = sorobanResources.writeBytes();
-    const readOnlyEntries = footprint.readOnly().length;
-    const readWriteEntries = footprint.readWrite().length;
-
-    return new Resources(
-      fee,
-      refundableFee,
-      cpuInst,
-      readBytes,
-      writeBytes,
-      readOnlyEntries,
-      readWriteEntries
-    );
-  }
-}
 
 export class ContractResponse<T> {
   result: Result<T, ContractError>;
   hash: string;
-  resources: Resources;
 
   private constructor(result?: Result<T, ContractError>) {
     if (result) {
@@ -126,19 +66,15 @@ export class ContractResponse<T> {
 
     if (SorobanRpc.Api.isSimulationError(simulation)) {
       response.result = new Err(parseError(simulation));
-      response.resources = new Resources(0, 0, 0, 0, 0, 0, 0);
     } else if (SorobanRpc.Api.isSimulationRestore(simulation)) {
       response.result = new Err(
         new ContractError(ContractErrorType.InvokeHostFunctionEntryArchived)
       );
-      response.resources = Resources.fromTransaction(transaction.toEnvelope());
     } else {
       if (!simulation.result) {
         response.result = new Err(new ContractError(ContractErrorType.UnknownError));
-        response.resources = new Resources(0, 0, 0, 0, 0, 0, 0);
       } else {
         response.result = new Ok(parser(simulation.result.retval!.toXDR('base64')));
-        response.resources = Resources.fromTransaction(transaction.toEnvelope());
       }
     }
     return response;
@@ -154,7 +90,6 @@ export class ContractResponse<T> {
     if (typeof transaction === 'string') {
       transaction = new Transaction(transaction, network_passphrase);
     }
-    response.resources = Resources.fromTransaction(transaction.toEnvelope());
     response.hash = transaction.hash().toString('hex');
 
     if (txResponse.status === SorobanRpc.Api.GetTransactionStatus.SUCCESS) {
