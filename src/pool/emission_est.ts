736bac1633b3fe212486c167aa4afdd9abedec64
mootz12 <mootz12@protonmail.com>
2024-05-05 13:19:09 -0400

    feat: add better emission estimation for positions without an emission object

--- src/pool/emission_est.ts
+++ src/pool/emission_est.ts
@@ -6,6 +6,7 @@ export class EmissionEstimates {
    */
   constructor(
     public emissions: Map<string, [number, number]>,
+    public tokenIdsToClaim: number[],
     public totalEmissions: number,
     public timestamp: number
   ) {}
@@ -17,43 +18,78 @@ export class EmissionEstimates {
     timestamp: number
   ): EmissionEstimates {
     // accrue emission values
-    // TODO: Refactor such that we catch emissions that are not created yet (user position created before emissions)
-    const reserve_list = pool.config.reserveList;
     const accrued_emissions = new Map<string, [number, number]>();
+    const tokenIdsToClaim: number[] = [];
     let totalEmissions = 0;
-    emissions.forEach((value, key) => {
-      const reserve = pool.reserves.get(reserve_list[Math.floor(key / 2)]);
-      if (reserve) {
-        if (key % 2 == 0 && reserve.borrowEmissions) {
-          // dToken emission
-          const dTokenAccrual = value.estimateAccrual(
+    for (const reserve of pool.reserves.values()) {
+      // handle dToken emissions
+      const d_token_id = reserve.config.index * 2;
+      const d_token_data = emissions.get(d_token_id);
+      const d_token_position = positions.liabilities.get(reserve.config.index);
+      if (reserve.borrowEmissions && (d_token_data || d_token_position)) {
+        let dTokenAccrual = 0;
+        if (d_token_data) {
+          dTokenAccrual = d_token_data.estimateAccrual(
             timestamp,
             reserve.borrowEmissions,
             reserve.config.decimals,
             reserve.data.dSupply,
-            positions.liabilities.get(reserve.config.index) ?? 0n
+            d_token_position ?? BigInt(0)
           );
+        } else if (d_token_position) {
+          // emissions began after user position was created, accrue all emissions
+          const temp_d_token_data = new PoolUserEmissionData(BigInt(0), BigInt(0));
+          dTokenAccrual = temp_d_token_data.estimateAccrual(
+            timestamp,
+            reserve.borrowEmissions,
+            reserve.config.decimals,
+            reserve.data.dSupply,
+            d_token_position
+          );
+        }
+        if (dTokenAccrual > 0) {
+          tokenIdsToClaim.push(d_token_id);
           totalEmissions += dTokenAccrual;
           const cur_value = accrued_emissions.get(reserve.assetId);
           accrued_emissions.set(reserve.assetId, [dTokenAccrual, cur_value ? cur_value[1] : 0]);
-        } else if (reserve.supplyEmissions) {
-          // bToken emission
-          const bTokenAccrual = value.estimateAccrual(
+        }
+      }
+
+      // handle bToken emissions
+      const b_token_index = reserve.config.index * 2 + 1;
+      const b_token_data = emissions.get(b_token_index);
+      const b_token_position =
+        (positions.collateral.get(reserve.config.index) ?? BigInt(0)) +
+        (positions.supply.get(reserve.config.index) ?? BigInt(0));
+      if (reserve.supplyEmissions && (b_token_data || b_token_position > BigInt(0))) {
+        let bTokenAccrual = 0;
+        if (b_token_data) {
+          bTokenAccrual = b_token_data.estimateAccrual(
             timestamp,
             reserve.supplyEmissions,
             reserve.config.decimals,
             reserve.data.bSupply,
-            (positions.collateral.get(reserve.config.index) ?? 0n) +
-              (positions.supply.get(reserve.config.index) ?? 0n)
+            b_token_position
           );
+        } else if (b_token_position > BigInt(0)) {
+          // emissions began after user position was created, accrue all emissions
+          const temp_b_token_data = new PoolUserEmissionData(BigInt(0), BigInt(0));
+          bTokenAccrual = temp_b_token_data.estimateAccrual(
+            timestamp,
+            reserve.supplyEmissions,
+            reserve.config.decimals,
+            reserve.data.bSupply,
+            b_token_position
+          );
+        }
+        if (bTokenAccrual > 0) {
+          tokenIdsToClaim.push(b_token_index);
           totalEmissions += bTokenAccrual;
           const cur_value = accrued_emissions.get(reserve.assetId);
           accrued_emissions.set(reserve.assetId, [cur_value ? cur_value[0] : 0, bTokenAccrual]);
         }
-      } else {
-        throw new Error(`Unable to find reserve for emissions: ${Math.floor(key / 2)}`);
       }
-    });
-    return new EmissionEstimates(accrued_emissions, totalEmissions, timestamp);
+    }
+    return new EmissionEstimates(accrued_emissions, tokenIdsToClaim, totalEmissions, timestamp);
   }
 }
