c01578fbdf9c85dbff63716c6089c2e05f463079
Ryang-21 <ryanyang2001@gmail.com>
2024-10-22 13:56:08 -0600

    implement auction class for scaling and Auctions interface for processing auction events

---
+++ src/pool/auction.ts
@@ -0,0 +1,167 @@
+import { FixedMath, i128 } from '../index.js';
+import { AuctionData, PoolEvent, PoolEventType } from './index.js';
+export enum AuctionType {
+  Liquidation = 0,
+  BadDebt = 1,
+  Interest = 2,
+}
+export interface Auctions {
+  /**
+   * Represents the filled auctions
+   */
+  filled: ScaledAuction[];
+  /**
+   * Represents the ongoing auctions
+   * @DEV The auctions must be scaled to the current block to accurately represent the ongoing auctions
+   */
+  ongoing: Auction[];
+}
+
+export interface ScaledAuction {
+  type: AuctionType;
+  user: string;
+  data: AuctionData;
+  startBlock: number;
+  scaleBlock: number;
+  filled: boolean;
+  fillHash: string | undefined;
+  fillBlock: number | undefined;
+}
+
+/**
+ * Creates an Auctions object from events representing the ongoing and filled auctions
+ * @param events - The events to create the scaled auctions from
+ * @param backstopId - The address of the backstop
+ * @returns An array of scaled auctions sorted by filled status and block
+ */
+export function getAuctionsfromEvents(events: PoolEvent[], backstopId: string): Auctions {
+  const auctions: Auctions = { filled: [], ongoing: [] };
+  for (const event of events ?? []) {
+    switch (event.eventType) {
+      case PoolEventType.NewLiquidationAuction:
+        auctions.ongoing.push(new Auction(event.user, AuctionType.Liquidation, event.auctionData));
+        break;
+      case PoolEventType.NewAuction:
+        auctions.ongoing.push(new Auction(backstopId, event.auctionType, event.auctionData));
+        break;
+      case PoolEventType.FillAuction: {
+        const index = auctions.ongoing.findIndex(
+          (auction) => auction.user === event.user && auction.type === event.auctionType
+        );
+        if (index !== -1) {
+          const auction = auctions.ongoing[index];
+          const [filledAuction, remainingAuction] = auction.scale(
+            event.ledger,
+            Number(event.fillAmount)
+          );
+          filledAuction.filled = true;
+          filledAuction.fillBlock = event.ledger;
+          filledAuction.fillHash = event.txHash;
+          auctions.filled.unshift(filledAuction);
+          if (remainingAuction !== undefined) {
+            auctions.ongoing[index] = remainingAuction;
+          } else {
+            auctions.ongoing.splice(index, 1);
+          }
+        }
+        break;
+      }
+      case PoolEventType.DeleteLiquidationAuction: {
+        const deleteIndex = auctions.ongoing.findIndex((auction) => auction.user === event.user);
+        if (deleteIndex !== -1) {
+          auctions.ongoing.splice(deleteIndex, 1);
+        }
+        break;
+      }
+    }
+  }
+  return auctions;
+}
+
+export class Auction {
+  user: string;
+  type: AuctionType;
+  data: AuctionData;
+
+  constructor(user: string, type: AuctionType, data: AuctionData) {
+    this.user = user;
+    this.type = type;
+    this.data = data;
+  }
+
+  /**
+   * Scale the auction to a given block and percentage filled
+   * @dev Rescaling a scaled auction will result in incorrect values
+   * @param scaleBlock - The block to scale the auction to
+   * @param scalePercent - The percentage of the auction to scale to (default 100)
+   * @returns Returns a tuple of the scaled auction and the remaining auction in that order
+   */
+  scale(scaleBlock: number, scalePercent = 100): [ScaledAuction, Auction | undefined] {
+    const scaledAuctionData: AuctionData = {
+      block: this.data.block,
+      bid: new Map(),
+      lot: new Map(),
+    };
+    const remainingAuctionData: AuctionData = {
+      block: this.data.block,
+      bid: new Map(),
+      lot: new Map(),
+    };
+    let lotModifier: bigint;
+    let bidModifier: bigint;
+    const fillBlockDelta = BigInt(scaleBlock - this.data.block);
+    const perBlockScaler = BigInt(50000);
+    const percentFillScaled = BigInt(scalePercent * 100000);
+
+    if (fillBlockDelta <= 200n) {
+      lotModifier = fillBlockDelta * perBlockScaler;
+      bidModifier = FixedMath.SCALAR_7;
+    } else {
+      lotModifier = FixedMath.SCALAR_7;
+      if (fillBlockDelta < 400n) {
+        bidModifier = FixedMath.SCALAR_7 - (fillBlockDelta - 200n) * perBlockScaler;
+      } else {
+        bidModifier = 0n;
+      }
+    }
+
+    for (const [assetId, amount] of Array.from(this.data.lot)) {
+      const baseFill = FixedMath.mulFloor(amount, percentFillScaled, FixedMath.SCALAR_7);
+      const remainingBase = amount - baseFill;
+      if (remainingBase > 0n) {
+        remainingAuctionData.lot.set(assetId, remainingBase);
+      }
+      const scaledLot = FixedMath.mulFloor(baseFill, lotModifier, FixedMath.SCALAR_7);
+      if (scaledLot > 0n) {
+        scaledAuctionData.lot.set(assetId, BigInt(scaledLot));
+      }
+    }
+
+    for (const [assetId, amount] of Array.from(this.data.bid)) {
+      const baseFill = FixedMath.mulCeil(amount, percentFillScaled, FixedMath.SCALAR_7);
+      const remainingBase = amount - baseFill;
+      if (remainingBase > 0n) {
+        remainingAuctionData.bid.set(assetId, remainingBase);
+      }
+      const scaledBid = FixedMath.mulCeil(baseFill, bidModifier, FixedMath.SCALAR_7);
+      if (scaledBid > 0n) {
+        scaledAuctionData.bid.set(assetId, BigInt(scaledBid));
+      }
+    }
+    let remainingAuction: Auction | undefined = undefined;
+    const scaledAuction: ScaledAuction = {
+      type: this.type,
+      user: this.user,
+      data: scaledAuctionData,
+      startBlock: this.data.block,
+      scaleBlock: scaleBlock,
+      filled: false,
+      fillHash: undefined,
+      fillBlock: undefined,
+    };
+    if (remainingAuctionData.lot.size > 0 || remainingAuctionData.bid.size > 0) {
+      remainingAuction = new Auction(this.user, this.type, remainingAuctionData);
+    }
+    return [scaledAuction, remainingAuction];
+  }
+}
---
+++ test/pool/auction.test.ts
@@ -0,0 +1,220 @@
+import { Auction, AuctionType, getAuctionsfromEvents } from '../../src/pool/auction.js';
+import { BlendContractType, PoolEvent, PoolEventType } from '../../src/index.js';
+import { i128 } from '../../src/index.js';
+
+describe('Auction', () => {
+  it('test auction scaling', () => {
+    const auction = new Auction('user', AuctionType.Liquidation, {
+      lot: new Map<string, bigint>([
+        ['asset2', 1_0000000n],
+        ['asset3', 5_0000001n],
+      ]),
+      bid: new Map<string, bigint>([
+        ['asset1', 100_0000000n],
+        ['asset2', 200_0000001n],
+      ]),
+      block: 123,
+    });
+
+    let [scaledAuction] = auction.scale(123, 100);
+    expect(scaledAuction.data.block).toEqual(123);
+    expect(scaledAuction.data.bid.size).toEqual(2);
+    expect(scaledAuction.data.bid.get('asset1')).toEqual(100_0000000n);
+    expect(scaledAuction.data.bid.get('asset2')).toEqual(200_0000001n);
+    expect(scaledAuction.data.lot.size).toEqual(0);
+
+    // 100 blocks -> 100 percent, validate lot is rounded down
+    [scaledAuction] = auction.scale(223, 100);
+    expect(scaledAuction.data.block).toEqual(123);
+    expect(scaledAuction.data.bid.size).toEqual(2);
+    expect(scaledAuction.data.bid.get('asset1')).toEqual(100_0000000n);
+    expect(scaledAuction.data.bid.get('asset2')).toEqual(200_0000001n);
+    expect(scaledAuction.data.lot.size).toEqual(2);
+    expect(scaledAuction.data.lot.get('asset2')).toEqual(5000000n);
+    expect(scaledAuction.data.lot.get('asset3')).toEqual(2_5000000n);
+
+    // 100 blocks -> 50 percent, validate bid is rounded up
+    [scaledAuction] = auction.scale(223, 50);
+    expect(scaledAuction.data.block).toEqual(123);
+    expect(scaledAuction.data.bid.size).toEqual(2);
+    expect(scaledAuction.data.bid.get('asset1')).toEqual(50_0000000n);
+    expect(scaledAuction.data.bid.get('asset2')).toEqual(100_0000001n);
+    expect(scaledAuction.data.lot.size).toEqual(2);
+    expect(scaledAuction.data.lot.get('asset2')).toEqual(2500000n);
+    expect(scaledAuction.data.lot.get('asset3')).toEqual(1_2500000n);
+
+    // 200 blocks -> 100 percent (is same)
+    [scaledAuction] = auction.scale(323, 100);
+    expect(scaledAuction.data.block).toEqual(123);
+    expect(scaledAuction.data.bid.size).toEqual(2);
+    expect(scaledAuction.data.bid.get('asset1')).toEqual(100_0000000n);
+    expect(scaledAuction.data.bid.get('asset2')).toEqual(200_0000001n);
+    expect(scaledAuction.data.lot.size).toEqual(2);
+    expect(scaledAuction.data.lot.get('asset2')).toEqual(1_0000000n);
+    expect(scaledAuction.data.lot.get('asset3')).toEqual(5_0000001n);
+
+    // 200 blocks -> 75 percent, validate bid is rounded up and lot is rounded down
+    [scaledAuction] = auction.scale(323, 75);
+    expect(scaledAuction.data.block).toEqual(123);
+    expect(scaledAuction.data.bid.size).toEqual(2);
+    expect(scaledAuction.data.bid.get('asset1')).toEqual(75_0000000n);
+    expect(scaledAuction.data.bid.get('asset2')).toEqual(150_0000001n);
+    expect(scaledAuction.data.lot.size).toEqual(2);
+    expect(scaledAuction.data.lot.get('asset2')).toEqual(7500000n);
+    expect(scaledAuction.data.lot.get('asset3')).toEqual(3_7500000n);
+
+    // 300 blocks -> 100 percent
+    [scaledAuction] = auction.scale(423, 100);
+    expect(scaledAuction.data.block).toEqual(123);
+    expect(scaledAuction.data.bid.size).toEqual(2);
+    expect(scaledAuction.data.bid.get('asset1')).toEqual(50_0000000n);
+    expect(scaledAuction.data.bid.get('asset2')).toEqual(100_0000001n);
+    expect(scaledAuction.data.lot.size).toEqual(2);
+    expect(scaledAuction.data.lot.get('asset2')).toEqual(1_0000000n);
+    expect(scaledAuction.data.lot.get('asset3')).toEqual(5_0000001n);
+
+    // 400 blocks -> 100 percent
+    [scaledAuction] = auction.scale(523, 100);
+    expect(scaledAuction.data.block).toEqual(123);
+    expect(scaledAuction.data.bid.size).toEqual(0);
+    expect(scaledAuction.data.lot.size).toEqual(2);
+    expect(scaledAuction.data.lot.get('asset2')).toEqual(1_0000000n);
+    expect(scaledAuction.data.lot.get('asset3')).toEqual(5_0000001n);
+
+    // 500 blocks -> 100 percent (unchanged)
+    [scaledAuction] = auction.scale(623, 100);
+    expect(scaledAuction.data.block).toEqual(123);
+    expect(scaledAuction.data.bid.size).toEqual(0);
+    expect(scaledAuction.data.lot.size).toEqual(2);
+    expect(scaledAuction.data.lot.get('asset2')).toEqual(1_0000000n);
+    expect(scaledAuction.data.lot.get('asset3')).toEqual(5_0000001n);
+  });
+
+  it('test auction scaling with 1 stroop', () => {
+    const auction = new Auction('user', AuctionType.Liquidation, {
+      lot: new Map<string, bigint>([['asset2', 1n]]),
+      bid: new Map<string, bigint>([['asset1', 1n]]),
+      block: 123,
+    });
+
+    // 1 blocks -> 10 percent
+    let [scaledAuction] = auction.scale(124, 10);
+    expect(scaledAuction.data.block).toEqual(123);
+    expect(scaledAuction.data.bid.size).toEqual(1);
+    expect(scaledAuction.data.bid.get('asset1')).toEqual(1n);
+    expect(scaledAuction.data.lot.size).toEqual(0);
+
+    // 399 blocks -> 10 percent
+    [scaledAuction] = auction.scale(522, 10);
+    expect(scaledAuction.data.block).toEqual(123);
+    expect(scaledAuction.data.bid.size).toEqual(1);
+    expect(scaledAuction.data.bid.get('asset1')).toEqual(1n);
+    expect(scaledAuction.data.lot.size).toEqual(0);
+
+    // 399 blocks -> 100 percent
+    [scaledAuction] = auction.scale(522, 100);
+    expect(scaledAuction.data.block).toEqual(123);
+    expect(scaledAuction.data.bid.size).toEqual(1);
+    expect(scaledAuction.data.bid.get('asset1')).toEqual(1n);
+    expect(scaledAuction.data.lot.size).toEqual(1);
+    expect(scaledAuction.data.lot.get('asset2')).toEqual(1n);
+  });
+});
+
+describe('getAuctionsFromEvents', () => {
+  it('should create an array of auctions from events', () => {
+    const events: PoolEvent[] = [
+      {
+        contractId: 'id',
+        contractType: BlendContractType.Pool,
+        id: 'id',
+        ledgerClosedAt: '100',
+        txHash: 'hash',
+        ledger: 100,
+        eventType: PoolEventType.NewLiquidationAuction,
+        user: 'user1',
+        auctionData: {
+          block: 100,
+          bid: new Map<string, i128>([['asset2', 100_000_0000n]]),
+          lot: new Map<string, i128>([
+            ['asset0', 10_000_0000n],
+            ['asset1', 1_000_0000n],
+          ]),
+        },
+      },
+      {
+        contractId: 'id',
+        contractType: BlendContractType.Pool,
+        id: 'id',
+        ledgerClosedAt: '100',
+        txHash: 'hash',
+        eventType: PoolEventType.FillAuction,
+        from: 'from',
+        user: 'user1',
+        auctionType: AuctionType.Liquidation as number,
+        fillAmount: 25n,
+        ledger: 200,
+      },
+      {
+        contractId: 'id',
+        contractType: BlendContractType.Pool,
+        id: 'id',
+        ledgerClosedAt: '100',
+        txHash: 'hash',
+        eventType: PoolEventType.FillAuction,
+        from: 'from',
+        user: 'user1',
+        auctionType: AuctionType.Liquidation as number,
+        fillAmount: 67n,
+        ledger: 300,
+      },
+    ];
+    const auctions = getAuctionsfromEvents(events, 'backstopId');
+    const expectedFill_1 = {
+      block: 100,
+      bid: new Map<string, i128>([['asset2', 25_000_0000n]]),
+      lot: new Map<string, i128>([
+        ['asset0', 1_250_0000n],
+        ['asset1', 1250000n],
+      ]),
+    };
+    const expectedFill_2 = {
+      block: 100,
+      bid: new Map<string, i128>([['asset2', 50_250_0000n]]),
+      lot: new Map<string, i128>([
+        ['asset0', 5_025_0000n],
+        ['asset1', 502_5000n],
+      ]),
+    };
+    const expectedRemaining = {
+      block: 100,
+      bid: new Map<string, i128>([['asset2', 24_7500000n]]),
+      lot: new Map<string, i128>([
+        ['asset0', 2_4750000n],
+        ['asset1', 2475000n],
+      ]),
+    };
+    expect(auctions.filled.length).toBe(2);
+    expect(auctions.ongoing.length).toBe(1);
+
+    const remainingAuction = auctions.ongoing[0];
+    expect(remainingAuction.data.bid).toEqual(expectedRemaining.bid);
+    expect(remainingAuction.data.lot).toEqual(expectedRemaining.lot);
+    expect(remainingAuction.type).toBe(AuctionType.Liquidation);
+    expect(remainingAuction.user).toBe('user1');
+
+    const firstFill = auctions.filled[1];
+    expect(firstFill.data.bid).toEqual(expectedFill_1.bid);
+    expect(firstFill.data.lot).toEqual(expectedFill_1.lot);
+    expect(firstFill.fillBlock).toBe(200);
+    expect(firstFill.fillHash).toBe('hash');
+    expect(firstFill.filled).toBe(true);
+
+    const secondFill = auctions.filled[0];
+    expect(secondFill.data.bid).toEqual(expectedFill_2.bid);
+    expect(secondFill.data.lot).toEqual(expectedFill_2.lot);
+    expect(secondFill.fillBlock).toBe(300);
+    expect(secondFill.fillHash).toBe('hash');
+    expect(secondFill.filled).toBe(true);
+  });
+});
